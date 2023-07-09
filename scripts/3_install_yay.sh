d2955c63bdb73beca4aed92f6fdf47cf38ad9cb9
OCSPG <th3spg@gmail.com>
2023-07-09 02:10:04 +0200

    change yay script

--- scripts/3_install_yay.sh
+++ scripts/3_install_yay.sh
@@ -4,21 +4,22 @@ set -e
 
 # Function to install yay
 install_yay() {
-    # Check if Color option is uncommented in pacman.conf
-    if ! grep -q '^#Color' /etc/pacman.conf; then
-        sudo sed -i 's/^#Color$/Color/' /etc/pacman.conf
-    fi
-
-    # Check if ParallelDownloads option is uncommented in pacman.conf
-    if ! grep -q '^#ParallelDownloads' /etc/pacman.conf; then
-        sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-    fi
 
     git clone https://aur.archlinux.org/yay.git
     (cd yay && makepkg -si)
 }
 
+# Function to install packages from a text file
+install_packages() {
+    IFS=' ' read -ra packages <<< "$(cat packages.txt)"
+    yay -S "${packages[@]}"
+}
+
 # Main script execution
 echo "Installing yay..."
 install_yay
+echo
+
+echo "Installing packages from the text file..."
+install_packages
 echo
